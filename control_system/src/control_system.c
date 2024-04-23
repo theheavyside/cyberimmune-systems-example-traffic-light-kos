0bcb9ee1c725fe3075ebca4a35599ac10bad8afe
Roman Ganin <ra_ganin@mail.ru>
2024-04-23 22:19:59 +0300

    feat: added extended security policy

--- control_system/src/control_system.c
+++ control_system/src/control_system.c
@@ -12,7 +12,7 @@
 
 #include <assert.h>
 
-#define MODES_NUM 9
+#define MODES_NUM 11
 
 
 /* Control system entity entry point. */
@@ -30,7 +30,9 @@ int main(int argc, const char *argv[])
         traffic_light_IMode_Direction1Red + traffic_light_IMode_Direction2Green,
         traffic_light_IMode_Direction1Red + traffic_light_IMode_Direction2Yellow,
         traffic_light_IMode_Direction1Yellow + traffic_light_IMode_Direction1Blink + traffic_light_IMode_Direction2Yellow + traffic_light_IMode_Direction2Blink,
-        traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction2Green // <-- try to forbid this via security policies
+        traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction2Green, // <-- try to forbid this via security policies
+        traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction1Yellow + traffic_light_IMode_Direction2Green,
+        traffic_light_IMode_Direction1Green + traffic_light_IMode_Direction1Yellow + traffic_light_IMode_Direction2Green + traffic_light_IMode_Direction2Yellow
     };
 
     fprintf(stderr, "Hello I'm ControlSystem\n");
@@ -82,7 +84,7 @@ int main(int argc, const char *argv[])
              * Print result value from response
              * (result is the output argument of the Mode method).
              */
-            fprintf(stderr, "result = %0x\n", (int) res.result);
+            fprintf(stderr, "request = %0x; result = %0x\n", (int)req.value, (int) res.result);
             /**
              * Include received result value into value argument
              * to resend to lights gpio in next iteration.
