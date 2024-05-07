421adabe1765157afa595847ade71b74f326e659
Roman Ganin <ra_ganin@mail.ru>
2024-05-07 22:47:16 +0300

    feat: added diagnostics entity

---
+++ diagnostics/src/diagnostics.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+
+/* Diagnostics entry point. */
+int main(int argc, const char *argv[])
+{
+    std::cerr << "Hello, I'm Diagnostics" << std::endl;
+    while (true);
+
+    return EXIT_SUCCESS;
+}
